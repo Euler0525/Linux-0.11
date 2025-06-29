96a003043cca4747e4fe50003d14659bb79b71fd
Euler0525 <euler0525@139.com>
2025-06-29 22:02:00 +0800

    chore: format memory.c

--- init/main.c
+++ init/main.c
@@ -52,7 +52,7 @@ extern long kernel_mktime(struct tm *tm);
 extern long startup_time;
 
 /*
- * This is set up by the setup-routine at boot-time
+ * This is set up by the setup-routine at boot-time(setup.s)
  */
 #define EXT_MEM_K (*(unsigned short *)0x90002)
 #define DRIVE_INFO (*(struct drive_info *)0x90080)
